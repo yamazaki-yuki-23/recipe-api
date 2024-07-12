a812485e8d3fe4ba268665d78598afda012a269e
yamazaki-yuki-23 <natural4083@gmail.com>
2024-07-12 18:35:53 +0900

    ディレクトリ名の変更

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+// app/not-found.tsx
+export default function NotFound() {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+}
