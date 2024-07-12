4bb37bfc4d75d73c704cb29be7e3b81e9cd67c73
yamazaki-yuki-23 <natural4083@gmail.com>
2024-07-12 18:17:48 +0900

    next.config.mjsにリダイレクトの設定を追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/recipes',
+        destination: '/api/recipes',
+        permanent: true,
+      },
+      {
+        source: '/recipes/:id',
+        destination: '/api/recipes/:id',
+        permanent: true,
+      },
+    ]
+  },
+};
 
 export default nextConfig;
